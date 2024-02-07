bfb2335f46482d065d0b35eec765041a5d4454e1
syagawa <satoruygw@gmail.com>
2024-02-07 12:33:10 +0900

    Add IIFE to getElementsComputedWidth.js

--- getElementsComputedWidth.js
+++ getElementsComputedWidth.js
@@ -1,15 +1,18 @@
-const elms_ = document.querySelectorAll("*");
-const arr = [];
-const elms = [...elms_, document, window];
-const targetWidth = 1000;
+(function(){
 
-const callback = (elm) => {
-   const cw = elm.clientWidth;
-   if(cw > targetWidth){
-       const cssw = elm.computedStyleMap().get("width");
-       const cssm_w = elm.computedStyleMap().get("min-width");
-       console.log(elm, `width: ${cw}px`, "css-width:", cssw, "css-min-width:", cssm_w);
-   }
-};
-
-elms.forEach(callback);
+    const elms_ = document.querySelectorAll("*");
+    const arr = [];
+    const elms = [...elms_, document, window];
+    const targetWidth = 1000;
+    
+    const callback = (elm) => {
+        const cw = elm.clientWidth;
+        if(cw > targetWidth){
+            const cssw = elm.computedStyleMap().get("width");
+            const cssm_w = elm.computedStyleMap().get("min-width");
+            console.log(elm, `width: ${cw}px`, "css-width:", cssw, "css-min-width:", cssm_w);
+        }
+    };
+    
+    elms.forEach(callback);
+})();
