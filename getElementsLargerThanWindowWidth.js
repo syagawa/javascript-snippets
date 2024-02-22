e617aaebd90abe6725a6c137b9ede9a5933cc56c
syagawa <satoruygw@gmail.com>
2024-02-22 12:47:19 +0900

    Add etElementsLargerThanWindowWidth.js

---
+++ getElementsLargerThanWindowWidth.js
@@ -0,0 +1,20 @@
+// run in browser console
+(function(){
+
+    const elms_ = document.querySelectorAll("*");
+    const arr = [];
+    const elms = [...elms_, document, window];
+    const targetWidth = window.innerWidth;
+    
+    const callback = (elm) => {
+        const cw = elm.clientWidth;
+        if(cw > targetWidth){
+            const cssw = elm.computedStyleMap().get("width");
+            const cssm_w = elm.computedStyleMap().get("min-width");
+            console.log(elm, `width: ${cw}px`, "css-width:", cssw, "css-min-width:", cssm_w);
+        }
+    };
+    
+    elms.forEach(callback);
+
+})();
