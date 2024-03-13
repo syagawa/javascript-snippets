5ecea771efe3141d1bcb423d38bacb306d544456
syagawa <satoruygw@gmail.com>
2024-03-13 12:38:14 +0900

    Add hideAllImages.js

---
+++ hideAllImages.js
@@ -0,0 +1,9 @@
+// run in browser console
+(function(){
+    const hideElement = (elm) => {
+        elm.style.display="none";
+        elm.style.backgroundImage="none !important";
+    };
+    document.querySelectorAll("img").forEach((e)=>{hideElement(e)});
+    document.querySelectorAll("*[class*=img]").forEach((e)=>{hideElement(e)});
+})();
