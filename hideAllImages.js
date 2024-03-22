c4bd07e604c42ca2c5896b3c4510fbd59514247d
syagawa <satoruygw@gmail.com>
2024-03-22 12:38:26 +0900

    Update hideAllImages

--- hideAllImages.js
+++ hideAllImages.js
@@ -7,4 +7,14 @@
     document.querySelectorAll("img").forEach((e)=>{hideElement(e)});
     document.querySelectorAll("*[class*=img]").forEach((e)=>{hideElement(e)});
     document.querySelectorAll("svg").forEach((e)=>{hideElement(e)});
+
+    document.querySelectorAll("iframe").forEach((e) => {
+        if(e.contentDocument){
+            e.contentDocument.querySelectorAll("img").forEach((e)=>{hideElement(e)});
+            e.contentDocument.querySelectorAll("*[class*=img]").forEach((e)=>{hideElement(e)});
+            e.contentDocument.querySelectorAll("svg").forEach((e)=>{hideElement(e)});
+        
+        }
+    });
+
 })();
