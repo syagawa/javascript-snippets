1211a507262ef0313f9a5e639365f0eb2f1677ca
syagawa <satoruygw@gmail.com>
2024-05-09 14:47:48 +0900

    Add excludeValues

--- getElementsCSSProperty.js
+++ getElementsCSSProperty.js
@@ -1,12 +1,17 @@
 // run in browser console
 (function(){
     const targetProperty = "z-index";
+    const excludeValues = ["auto"];
     const elms_ = document.querySelectorAll("*");
     const elms = [...elms_, document, window];
     const callback = (elm) => {
         try{
-            const value = elm.computedStyleMap().get(targetProperty);
+            const target = elm.computedStyleMap().get(targetProperty);
+            const value = target.value;
             if(value){
+                if(excludeValues.includes(value)){
+                    return;
+                }
                 console.log(elm, `${targetProperty}: ${value}`);
             }
         }catch(e){
